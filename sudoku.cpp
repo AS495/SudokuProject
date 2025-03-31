390c04648ca7a3b1d1eb2eda41daec383db26de0
Anthony Schnuriger <anthonyschnuriger@gmail.com>
2025-03-31 17:05:18 -0500

    Test 2

--- sudoku.cpp
+++ sudoku.cpp
@@ -1,7 +1,8 @@
-#include <iostream>
+
 // Anthony Schnuriger
 // Eventually creating a sudoku solving game
 using namespace std;
+#include <iostream>
 #include <vector>
 
 bool numSafe(vector<vector<int>>& grid, int row, int col, int num) {
@@ -56,15 +57,15 @@ int main(){
   vector<vector<int>> matrix;
   // Example Matrix can and will be changed
   matrix = {
-    {3, 0, 6, 5, 0, 8, 4, 0, 0},
-    {5, 2, 0, 0, 0, 0, 0, 0, 0},
-    {0, 8, 7, 0, 0, 0, 0, 3, 1},
-    {0, 0, 3, 0, 1, 0, 0, 8, 0},
-    {9, 0, 0, 8, 6, 3, 0, 0, 5},
-    {0, 5, 0, 0, 9, 0, 6, 0, 0},
-    {1, 3, 0, 0, 0, 0, 2, 5, 0},
-    {0, 0, 0, 0, 0, 0, 0, 7, 4},
-    {0, 0, 5, 2, 0, 6, 3, 0, 0}};
+    {0, 0, 0, 2, 6, 0, 7, 0, 1},
+    {6, 8, 0, 0, 7, 0, 0, 9, 0},
+    {1, 9, 7, 0, 0, 4, 5, 3, 1},
+    {8, 2, 0, 1, 0, 0, 0, 4, 0},
+    {0, 0, 4, 6, 0, 2, 9, 0, 5},
+    {0, 5, 0, 0, 0, 3, 0, 2, 8},
+    {0, 0, 9, 3, 0, 0, 0, 7, 4},
+    {0, 4, 0, 0, 5, 0, 0, 3, 6},
+    {7, 0, 3, 0, 1, 8, 0, 0, 0}};
 
     solve(matrix, 0, 0);
     for (int i = 0; i < matrix.size(); i++) {
