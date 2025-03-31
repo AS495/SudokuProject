cbd520e50d01bd872563fbab8cf1d56d9a97c797
Anthony Schnuriger <anthonyschnuriger@gmail.com>
2025-03-31 16:28:29 -0500

    Using Backtracking to find numbers

--- sudoku.cpp
+++ sudoku.cpp
@@ -1,13 +1,31 @@
 // Anthony Schnuriger
 // Eventually creating a sudoku solving game
 using namespace std;
-#include <iostream>
 #include <vector>
 
-
+bool numSafe(vector<vector<int>>& grid, int row, int col, int num) {
+    for (int i = 0; i <= 8; i++) {
+        if (grid[row][i] == num) {
+            return false;
+        }
+        if (grid[i][col] == num) {
+            return false;
+        }
+    }
+    int threeMatRow = row - (row % 3);
+    int threeMatCol = col - (col % 3);
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (grid[i+threeMatRow][j+threeMatCol] == num) {
+                return false;
+            }
+        }
+    }
+}
 
 int main(){
   vector<vector<int>> matrix;
+  // Example Matrix can and will be changed
   matrix = {
     {3, 0, 6, 5, 0, 8, 4, 0, 0},
     {5, 2, 0, 0, 0, 0, 0, 0, 0},
